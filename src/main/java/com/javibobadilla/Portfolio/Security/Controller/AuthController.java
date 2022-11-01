2d63f6cd48129bcc450409a7a72547600e3da92f
Javier Bobadilla <javibobadilla@gmail.com>
2022-10-31 21:16:48 -0300

    Update AuthController.java

--- src/main/java/com/javibobadilla/Portfolio/Security/Controller/AuthController.java
+++ src/main/java/com/javibobadilla/Portfolio/Security/Controller/AuthController.java
@@ -31,7 +31,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4200/")
+/*@CrossOrigin(origins = "http://localhost:4200/")*/
+@CrossOrigin(origins = "https://miportfolio-frontend.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
